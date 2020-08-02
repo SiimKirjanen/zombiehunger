fed8f02570c56eac4c7b6cd98cda93627bcebc66
Siim <siim.kirjanen@gmail.com>
2020-08-02 16:43:13 +0300

    v3

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 var canvas = document.getElementById("taust");	//otsetee taust elemendile
-var tausta_canvas = canvas.getContext('2d');	//vaja tausta canvase muutmiseks jne...
+var tausta_canvas = canvas.getContext('2d');	//vaja tausta canvase muutmiseks jne...sinna tekivad tagataustad, menud jne...
 
 var canvas2 = document.getElementById("tegelane");
-var tegelase_canvas = canvas2.getContext('2d');		//vaja tegelease canvase muutmiseks (sellel liigub kangelane)
+var tegelase_canvas = canvas2.getContext('2d');		//vaja tegelease canvase muutmiseks (sellel liigub kangelane) 
 
 var canvas3 = document.getElementById("zombid");
 var zombid_taust= canvas3.getContext('2d');		//vaja zombide tausta muutmiseks (zombied liiguvad siin)
+
+var canvas4 = document.getElementById("abi");
+var abi_taust = canvas4.getContext('2d'); //siin on abi asjad. lisakuulid jne...
 
 var gameWidth = canvas.width;    // mängu laius
 var gameHeight = canvas.height;	 //mängu kõrgus
 
 
 
-var isPlaying = false;	
+
+var isPlaying = false;	//sellega saab m2ngu loopi kinni panna
 var requestAnimFrame =  window.requestAnimationFrame ||
                         window.webkitRequestAnimationFrame ||
                         window.mozRequestAnimationFrame ||
@@ -27,11 +31,11 @@ var imgSprite = new Image(); //pilt
 imgSprite.src = 'Pildid/sprite.png'; //pildi asukoht
 imgSprite.addEventListener('load',init,false); //pildi kuular. kui laetud siis init funktsioon
 
-
-
+var help = new Array(new Moon()); //help arrays on abistavad asjad m2ngus (kuulid ja ehk kunagi mingi elud jne)
 //*********Põhifunktsioonid**********//
 
 function init(){
+    
 	document.addEventListener('click',mouseClicked,false);
 	
 	 
@@ -41,6 +45,7 @@ function init(){
 function playGame(){
 	drawBg();
 	startLoop();
+	//sound.play();
 	document.addEventListener('keydown',checkKeyDown,false);
 	document.addEventListener('keyup',checkKeyUp,false);
 } //joonistatakse tagataust ja kutsutakse välja startLoop funktsioon, lisatakse kuularid
@@ -61,12 +66,21 @@ function startLoop(){
 function loop(){
     if(isPlaying){
       ise.draw();
+	  drawAllHelp();
       requestAnimFrame(loop);  
     }	
 }//põhi funktsioon, mis joonistab kangelast
-
+function drawAllHelp(){
+    clearAbi();
+	for(var i = 0; i < help.length;i++){
+		help[i].draw();
+	}
+}
 function stopLoop(){
     isPlaying = false;	
+}
+function clearAbi(){
+	abi_taust.clearRect(0,0,800,500);
 }
 
 //*********Põhifunktsioonide lõpp*********//
@@ -74,10 +88,10 @@ function stopLoop(){
 //********Hero funktsioonid**********//
 
 function Hero(){
-	this.srcX = 6;
-	this.srcY = 516;
+	this.srcX = 54;
+	this.srcY = 518;
 	this.width = 30;
-	this.height = 32;
+	this.height = 50;
 	this.speed = 2;
 	this.drawX = 200;
 	this.drawY = 200;
@@ -96,7 +110,7 @@ function Hero(){
 	this.isShooting = false;
 	this.bullets = [];
 	this.currentBullet = 0;
-	for(var i = 0; i < 5; i++){
+	for(var i = 0; i < 20; i++){
 		this.bullets[this.bullets.length] = new Bullet(this);	
 	}
 } //hero class
@@ -107,21 +121,32 @@ Hero.prototype.draw = function(){
 	this.checkDirection(); //muutab hero x ja y kordinaate, et saaks toimuda liikumine
 	switch(this.vaade){ //kuhu poole hero vaatab
 	    case 2: //paremale
-			tegelase_canvas.drawImage(imgSprite,6,581,this.width,this.height,this.drawX,this.drawY,this.width,this.height);
+			tegelase_canvas.drawImage(imgSprite,85,518,40,this.height,this.drawX,this.drawY,40,this.height);
 			break;
 		case 3: //alla
-			tegelase_canvas.drawImage(imgSprite,6,516,this.width,this.height,this.drawX,this.drawY,this.width,this.height);
+			tegelase_canvas.drawImage(imgSprite,54,518,this.width,this.height,this.drawX,this.drawY,this.width,this.height);
 			break;
 		case 1: //ylesse
-			tegelase_canvas.drawImage(imgSprite,6,613,this.width,this.height,this.drawX,this.drawY,this.width,this.height);
+			tegelase_canvas.drawImage(imgSprite,130,518,30,this.height,this.drawX,this.drawY,30,this.height);
 			break;
 		case 4: //vasakule
-			tegelase_canvas.drawImage(imgSprite,6,549,this.width,this.height,this.drawX,this.drawY,this.width,this.height);
+			tegelase_canvas.drawImage(imgSprite,6,518,40,this.height,this.drawX,this.drawY,40,this.height);
 			break;
 	}
 	this.kustutamisele(); //vaatab koik kuulid läbi ja kustutab need mis on välja lastud ja märgitud kustutamisele
 	this.checkShooting(); //kui space on vajutatud vaatab kuulide mas ja kui leiab vaja kuuli muutab kuuli x kordinaati.
 	this.drawAllBullets(); //vaatab koik kuulid l2bi ja need millel on x kordinaat suurem kui 0 neid hakkab joonistama.	
+	this.checkHelp();
+};
+Hero.prototype.checkHelp = function(){
+	for(var i = 0; i < help.length;i++){
+		if(this.drawY < help[i].drawY){
+		    var a = document.getElementById("info");
+			a.value = "Pihtsa";
+			ise.bullets.push(new Bullet());
+			help.splice(i,1);
+		}
+	}
 };
 Hero.prototype.updateCoors = function(){
     this.noseX = this.drawX + 30; //kust kohast kuul v2lja saata
@@ -164,12 +189,15 @@ Hero.prototype.checkShooting = function(){
 			   if(this.bullets[i].suund == 1){
 				 this.noseX = this.drawX + 15;
 				 this.noseY = this.drawY - 10;
+			   }else if(this.bullets[i].suund == 2){
+			     this.noseX = this.drawX + 32;
+				 this.noseY = this.drawY + 22;
 			   }else if(this.bullets[i].suund == 3){
-				 this.noseX = this.drawX + 15;
-				 this.noseY = this.drawY + 20;
+				 this.noseX = this.drawX + 8;
+				 this.noseY = this.drawY + 45;
 			   }else if(this.bullets[i].suund == 4){
 			     this.noseX = this.drawX;
-				 this.noseY = this.drawY + 15;
+				 this.noseY = this.drawY + 22;
 			   }
 			   this.bullets[i].fire(this.noseX, this.noseY);
 			   return;
@@ -206,12 +234,12 @@ Hero.prototype.drawAllBullets = function(){
 
 function Bullet(j){ 
 	this.hero = j;
-	this.srcX = 15;
-	this.srcY = 654;
+	this.srcX = 8;
+	this.srcY = 576;
 	this.drawX = -20;
 	this.drawY = 0;
-	this.width = 10;
-	this.height = 10;
+	this.width = 8;
+	this.height = 8;
 	this.kasutuses = false;
 	this.kustutamisele = false;
 	this.suund = 2;
@@ -245,7 +273,23 @@ Bullet.prototype.fire= function(startX, startY){
 };
 //****kuuli funktsioonide lopp******//
 
+//*****reload funktsioonid*******//
 
+function Moon(){
+    //var randomnumber=Math.floor(Math.random()*11)
+	this.srcX = 10;
+	this.srcY = 576;
+	this.drawX = Math.floor(Math.random()*800);
+	this.drawY = 50;
+	this.width = 6;
+	this.height = 6;
+}
+Moon.prototype.draw = function(){
+	tegelase_canvas.drawImage(imgSprite,this.srcX,this.srcY,this.width,this.height,this.drawX,this.drawY,this.width,this.height);
+};
+
+
+//*****end of reload funktsioonid*****//
 
 
 //*********Event funktsioonid**********//
@@ -277,7 +321,7 @@ function checkKeyDown(e){
   if(keyID === 37 || keyID === 65){ // 38 means left arrow and 87 means a key
     ise.vaade = 4;
     ise.isLeftKey = true;
-	ise.bullets.push(new Bullet());
+	
 	e.preventDefault();
   }
   if(keyID === 32){ // 32 space
